69d291b9f13d845d891b50563de138617d4f0581
George Doherty <Georgejamesdoherty@gmail.com>
2022-10-06 23:54:15 +0100

    Start of API implementation. Controller update, service and project files

--- DemoProject/Controllers/ApiController.cs
+++ DemoProject/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoProject.Services.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,16 @@ namespace DemoProject.Controllers
     public class ApiController : Controller
     {
 
-        public ApiController()
-        {
+        private readonly ApiServices _apiServices;
 
+        public ApiController(ApiServices apiServices)
+        {
+            _apiServices = apiServices;
         }
 
         public IActionResult Index()
         {
+            _apiServices.Setup();
             return View();
         }
     }
--- Services/Services/ApiServices.cs
+++ Services/Services/ApiServices.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using DemoProject.Services.ApiModels;
+using DemoProject.Services.Data;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace DemoProject.Services.Services
 {
-    class ApiServices
+    public class ApiServices
     {
         //https://docs.openaq.org/v2/ <-- test api used
+
+        private const string BaseUrl = "https://api.openaq.org/v2/";
+        private readonly DemoContext _demoContext;
+        public ApiServices(DemoContext demoContext)
+        {
+            _demoContext = demoContext;
+        }
+
+        private T HttpRequest<T>(string resource, List<(string, string)> parameters )
+        {
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest(resource, Method.Get);
+            foreach (var parameter in parameters) 
+            {
+                request.AddParameter(parameter.Item1, parameter.Item2);
+            }
+            request.AddHeader("accept", "application/json");
+            var response = client.Execute(request).Content;
+            var responseAsVar = JsonSerializer.Deserialize<T>(response);
+            return responseAsVar;
+        }
+
+        public void Setup()
+        {
+            var paramters = new List<(string, string)>()
+            {
+                ("limit", "2"),
+                ("page", "1")
+            };
+            var cityList = HttpRequest<CityInfoList>("cities", paramters);
+        }
+
+
+        //class to deserialize Json for cities
+        private class CityInfoList
+        {
+            public List<CityInfo> results { get; set; }
+        }
     }
 }
