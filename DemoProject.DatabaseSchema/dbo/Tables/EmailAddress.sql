0471922ddc09fa8c8106ea1f103f826b04eb460c
George Doherty <Georgejamesdoherty@gmail.com>
2022-10-06 01:57:00 +0100

    Added Database Project file

---
+++ DemoProject.DatabaseSchema/dbo/Tables/EmailAddress.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EmailAddress] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [PersonId]  INT           NULL,
+    [Email]     NVARCHAR (50) NULL,
+    [IsActive]  BIT           DEFAULT ((1)) NULL,
+    [DateAdded] DATETIME      DEFAULT (getutcdate()) NULL,
+    FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id])
+);
+
