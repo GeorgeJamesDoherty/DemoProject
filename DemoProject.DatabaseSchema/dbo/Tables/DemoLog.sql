dade20c6d5768ae8d8dc465c01d1d787dec851e6
George Doherty <Georgejamesdoherty@gmail.com>
2022-10-18 23:10:08 +0100

    Added basic logging functionality

---
+++ DemoProject.DatabaseSchema/dbo/Tables/DemoLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DemoLog] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Message]         NVARCHAR (MAX) NULL,
+    [MessageTemplate] NVARCHAR (MAX) NULL,
+    [Level]           NVARCHAR (128) NULL,
+    [TimeStamp]       DATETIME       NOT NULL,
+    [Exception]       NVARCHAR (MAX) NULL,
+    [Properties]      NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Services/Services/Logger.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using Serilog;
+
+namespace DemoProject.Services.Services
+{
+    public static class Logger
+    {
+        private static Serilog.Core.Logger log;
+
+        static Logger()
+        {
+            try
+            {
+                Serilog.Debugging.SelfLog.Enable(msg => System.Diagnostics.Debug.WriteLine(msg));
+
+                var _configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                var defaultLogLevel = _configuration["Serilog:MinimumLevel"];
+                var connectionString = _configuration.GetConnectionString("DemoContext").ToString();
+
+                var logLevel = (LogEventLevel)Enum.Parse(typeof(LogEventLevel), defaultLogLevel);
+                var levelSwitch = new LoggingLevelSwitch(logLevel);
+
+                log = new LoggerConfiguration()
+                    .WriteTo.MSSqlServer(connectionString, sinkOptions: new MSSqlServerSinkOptions { TableName = "DemoLog", AutoCreateSqlTable = true })
+                    .MinimumLevel.ControlledBy(levelSwitch)
+                    .Enrich.WithProperty("Application", "Demo Project")
+                    .CreateLogger();
+
+
+                System.Diagnostics.Debug.WriteLine("");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void Info(string message)
+        {
+            log.Information(message);
+        }
+
+        public static void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public static void Error(Exception ex, string message)
+        {
+            log.Error(ex, message);
+        }
+
+        public static void Fatal(string message)
+        {
+            log.Fatal(message);
+        }
+
+        public static void Fatal(Exception ex, string message)
+        {
+            log.Fatal(ex, message);
+        }
+    }
+}
